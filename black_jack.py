a0098e7a723806942904ee72c52f42b7929e8052
Jesse Dorman <Jesse.dorman@us.af.mil>
2022-10-13 19:46:56 +0000

    write logic for ace card values

--- black_jack.py
+++ black_jack.py
@@ -4,7 +4,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 import random
 
 
-deck = {"1♠": ["black/Pikes_A_black.png", 1],"2♠": ["black/Pikes_2_black.png", 2],"3♠": ["black/Pikes_3_black.png", 3],"4♠": ["black/Pikes_4_black.png", 4],"5♠": ["black/Pikes_5_black.png", 5],"6♠": ["black/Pikes_6_black.png", 6],"7♠": ["black/Pikes_7_black.png", 7],"8♠": ["black/Pikes_8_black.png", 8],"9♠": ["black/Pikes_9_black.png", 9],"10♠": ["black/Pikes_10_black.png", 10],"J♠": ["black/Pikes_Jack_black.png", 10],"Q♠": ["black/Pikes_Queen_black.png", 10],"K♠": ["black/Pikes_King_black.png", 10],"1♥": ["black/Hearts_A_black.png", 1],"2♥": ["black/Hearts_2_black.png", 2],"3♥": ["black/Hearts_3_black.png", 3],"4♥": ["black/Hearts_4_black.png", 4],"5♥": ["black/Hearts_5_black.png", 5],"6♥": ["black/Hearts_6_black.png", 6],"7♥": ["black/Hearts_7_black.png", 7],"8♥": ["black/Hearts_8_black.png", 8],"9♥": ["black/Hearts_9_black.png", 9],"10♥": ["black/Hearts_10_black.png", 10],"J♥": ["black/Hearts_Jack_black.png", 10],"Q♥": ["black/Hearts_Queen_black.png", 10],"K♥": ["black/Hearts_King_black.png", 10],"1♣": ["black/Clovers_A_black.png", 1],"2♣": ["black/Clovers_2_black.png", 2],"3♣": ["black/Clovers_3_black.png", 3],"4♣": ["black/Clovers_4_black.png", 4],"5♣": ["black/Clovers_5_black.png", 5],"6♣": ["black/Clovers_6_black.png", 6],"7♣": ["black/Clovers_7_black.png", 7],"8♣": ["black/Clovers_8_black.png", 8],"9♣": ["black/Clovers_9_black.png", 9],"10♣": ["black/Clovers_10_black.png", 10],"J♣": ["black/Clovers_Jack_black.png", 10],"Q♣": ["black/Clovers_Queen_black.png", 10],"K♣": ["black/Clovers_King_black.png", 10],"1♦": ["black/Tiles_A_black.png", 1],"2♦": ["black/Tiles_2_black.png", 2],"3♦": ["black/Tiles_3_black.png", 3],"4♦": ["black/Tiles_4_black.png", 4],"5♦": ["black/Tiles_5_black.png", 5],"6♦": ["black/Tiles_6_black.png", 6],"7♦": ["black/Tiles_7_black.png", 7],"8♦": ["black/Tiles_8_black.png", 8],"9♦": ["black/Tiles_9_black.png", 9],"10♦": ["black/Tiles_10_black.png", 10],"J♦": ["black/Tiles_Jack_black.png", 10],"Q♦": ["black/Tiles_Queen_black.png", 10],"K♦": ["black/Tiles_King_black.png", 10]}
+deck = {"1♠": ["black/Pikes_A_black.png", 11],"2♠": ["black/Pikes_2_black.png", 2],"3♠": ["black/Pikes_3_black.png", 3],"4♠": ["black/Pikes_4_black.png", 4],"5♠": ["black/Pikes_5_black.png", 5],"6♠": ["black/Pikes_6_black.png", 6],"7♠": ["black/Pikes_7_black.png", 7],"8♠": ["black/Pikes_8_black.png", 8],"9♠": ["black/Pikes_9_black.png", 9],"10♠": ["black/Pikes_10_black.png", 10],"J♠": ["black/Pikes_Jack_black.png", 10],"Q♠": ["black/Pikes_Queen_black.png", 10],"K♠": ["black/Pikes_King_black.png", 10],"1♥": ["black/Hearts_A_black.png", 11],"2♥": ["black/Hearts_2_black.png", 2],"3♥": ["black/Hearts_3_black.png", 3],"4♥": ["black/Hearts_4_black.png", 4],"5♥": ["black/Hearts_5_black.png", 5],"6♥": ["black/Hearts_6_black.png", 6],"7♥": ["black/Hearts_7_black.png", 7],"8♥": ["black/Hearts_8_black.png", 8],"9♥": ["black/Hearts_9_black.png", 9],"10♥": ["black/Hearts_10_black.png", 10],"J♥": ["black/Hearts_Jack_black.png", 10],"Q♥": ["black/Hearts_Queen_black.png", 10],"K♥": ["black/Hearts_King_black.png", 10],"1♣": ["black/Clovers_A_black.png", 11],"2♣": ["black/Clovers_2_black.png", 2],"3♣": ["black/Clovers_3_black.png", 3],"4♣": ["black/Clovers_4_black.png", 4],"5♣": ["black/Clovers_5_black.png", 5],"6♣": ["black/Clovers_6_black.png", 6],"7♣": ["black/Clovers_7_black.png", 7],"8♣": ["black/Clovers_8_black.png", 8],"9♣": ["black/Clovers_9_black.png", 9],"10♣": ["black/Clovers_10_black.png", 10],"J♣": ["black/Clovers_Jack_black.png", 10],"Q♣": ["black/Clovers_Queen_black.png", 10],"K♣": ["black/Clovers_King_black.png", 10],"1♦": ["black/Tiles_A_black.png", 11],"2♦": ["black/Tiles_2_black.png", 2],"3♦": ["black/Tiles_3_black.png", 3],"4♦": ["black/Tiles_4_black.png", 4],"5♦": ["black/Tiles_5_black.png", 5],"6♦": ["black/Tiles_6_black.png", 6],"7♦": ["black/Tiles_7_black.png", 7],"8♦": ["black/Tiles_8_black.png", 8],"9♦": ["black/Tiles_9_black.png", 9],"10♦": ["black/Tiles_10_black.png", 10],"J♦": ["black/Tiles_Jack_black.png", 10],"Q♦": ["black/Tiles_Queen_black.png", 10],"K♦": ["black/Tiles_King_black.png", 10]}
 
 keys = list(deck.keys())
 random.shuffle(keys)
@@ -35,17 +35,31 @@ class GameScreen(Screen):
         self.ids.hit_button.disabled = False
         self.ids.stay_button.disabled = False
 
-        player = sum(player)
-        dealer = sum(dealer)
-
+  
         self.check_score(player,dealer)
     
     
 
     def check_score(self, player, dealer):
+
+        player_score = sum(player)
+        dealer_score = sum(dealer)
+
+        for values in player:
+            if values == 11 and player_score > 21:
+                player.remove(11)
+                player.append(1)
+                player_score = sum(player)
+
+        for value in dealer:
+            if value == 11 and dealer_score > 21:
+                dealer.remove(11)
+                dealer.append(1)
+                dealer_score = sum(dealer)
+
         
-        if player == 21 and player == dealer:
-            print("Its a tie!")
+        # if player_score == 21 and player_score == dealer_score:
+        #     print("Its a tie!")
 
 
 

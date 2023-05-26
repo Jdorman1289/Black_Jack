1a5332e193c55239966aab5a3bbe15e6db1b5a98
Jesse Dorman <Jdorman1289@gmail.com>
2023-05-26 06:58:33 -0500

    refactor various pep8 items to make code more readable

--- black_jack.py
+++ black_jack.py
@@ -5,8 +5,32 @@ from kivymd.uix.dialog import MDDialog
 import webbrowser
 import random
 
-
-deck = {"1♠": ["Playing-Cards/card-spades-1.png", 11],"2♠": ["Playing-Cards/card-spades-2.png", 2],"3♠": ["Playing-Cards/card-spades-3.png", 3],"4♠": ["Playing-Cards/card-spades-4.png", 4],"5♠": ["Playing-Cards/card-spades-5.png", 5],"6♠": ["Playing-Cards/card-spades-6.png", 6],"7♠": ["Playing-Cards/card-spades-7.png", 7],"8♠": ["Playing-Cards/card-spades-8.png", 8],"9♠": ["Playing-Cards/card-spades-9.png", 9],"10♠": ["Playing-Cards/card-spades-10.png", 10],"J♠": ["Playing-Cards/card-spades-11.png", 10],"Q♠": ["Playing-Cards/card-spades-12.png", 10],"K♠": ["Playing-Cards/card-spades-13.png", 10],"1♥": ["Playing-Cards/card-hearts-1.png", 11],"2♥": ["Playing-Cards/card-hearts-2.png", 2],"3♥": ["Playing-Cards/card-hearts-3.png", 3],"4♥": ["Playing-Cards/card-hearts-4.png", 4],"5♥": ["Playing-Cards/card-hearts-5.png", 5],"6♥": ["Playing-Cards/card-hearts-6.png", 6],"7♥": ["Playing-Cards/card-hearts-7.png", 7],"8♥": ["Playing-Cards/card-hearts-8.png", 8],"9♥": ["Playing-Cards/card-hearts-9.png", 9],"10♥": ["Playing-Cards/card-hearts-10.png", 10],"J♥": ["Playing-Cards/card-hearts-11.png", 10],"Q♥": ["Playing-Cards/card-hearts-12.png", 10],"K♥": ["Playing-Cards/card-hearts-13.png", 10],"1♣": ["Playing-Cards/card-clubs-1.png", 11],"2♣": ["Playing-Cards/card-clubs-2.png", 2],"3♣": ["Playing-Cards/card-clubs-3.png", 3],"4♣": ["Playing-Cards/card-clubs-4.png", 4],"5♣": ["Playing-Cards/card-clubs-5.png", 5],"6♣": ["Playing-Cards/card-clubs-6.png", 6],"7♣": ["Playing-Cards/card-clubs-7.png", 7],"8♣": ["Playing-Cards/card-clubs-8.png", 8],"9♣": ["Playing-Cards/card-clubs-9.png", 9],"10♣": ["Playing-Cards/card-clubs-10.png", 10],"J♣": ["Playing-Cards/card-clubs-11.png", 10],"Q♣": ["Playing-Cards/card-clubs-12.png", 10],"K♣": ["Playing-Cards/card-clubs-13.png", 10],"1♦": ["Playing-Cards/card-diamonds-1.png", 11],"2♦": ["Playing-Cards/card-diamonds-2.png", 2],"3♦": ["Playing-Cards/card-diamonds-3.png", 3],"4♦": ["Playing-Cards/card-diamonds-4.png", 4],"5♦": ["Playing-Cards/card-diamonds-5.png", 5],"6♦": ["Playing-Cards/card-diamonds-6.png", 6],"7♦": ["Playing-Cards/card-diamonds-7.png", 7],"8♦": ["Playing-Cards/card-diamonds-8.png", 8],"9♦": ["Playing-Cards/card-diamonds-9.png", 9],"10♦": ["Playing-Cards/card-diamonds-10.png", 10],"J♦": ["Playing-Cards/card-diamonds-11.png", 10],"Q♦": ["Playing-Cards/card-diamonds-12.png", 10],"K♦": ["Playing-Cards/card-diamonds-13.png", 10]}
+deck = {"1♠": ["Playing-Cards/card-spades-1.png", 11], "2♠": ["Playing-Cards/card-spades-2.png", 2],
+        "3♠": ["Playing-Cards/card-spades-3.png", 3], "4♠": ["Playing-Cards/card-spades-4.png", 4],
+        "5♠": ["Playing-Cards/card-spades-5.png", 5], "6♠": ["Playing-Cards/card-spades-6.png", 6],
+        "7♠": ["Playing-Cards/card-spades-7.png", 7], "8♠": ["Playing-Cards/card-spades-8.png", 8],
+        "9♠": ["Playing-Cards/card-spades-9.png", 9], "10♠": ["Playing-Cards/card-spades-10.png", 10],
+        "J♠": ["Playing-Cards/card-spades-11.png", 10], "Q♠": ["Playing-Cards/card-spades-12.png", 10],
+        "K♠": ["Playing-Cards/card-spades-13.png", 10], "1♥": ["Playing-Cards/card-hearts-1.png", 11],
+        "2♥": ["Playing-Cards/card-hearts-2.png", 2], "3♥": ["Playing-Cards/card-hearts-3.png", 3],
+        "4♥": ["Playing-Cards/card-hearts-4.png", 4], "5♥": ["Playing-Cards/card-hearts-5.png", 5],
+        "6♥": ["Playing-Cards/card-hearts-6.png", 6], "7♥": ["Playing-Cards/card-hearts-7.png", 7],
+        "8♥": ["Playing-Cards/card-hearts-8.png", 8], "9♥": ["Playing-Cards/card-hearts-9.png", 9],
+        "10♥": ["Playing-Cards/card-hearts-10.png", 10], "J♥": ["Playing-Cards/card-hearts-11.png", 10],
+        "Q♥": ["Playing-Cards/card-hearts-12.png", 10], "K♥": ["Playing-Cards/card-hearts-13.png", 10],
+        "1♣": ["Playing-Cards/card-clubs-1.png", 11], "2♣": ["Playing-Cards/card-clubs-2.png", 2],
+        "3♣": ["Playing-Cards/card-clubs-3.png", 3], "4♣": ["Playing-Cards/card-clubs-4.png", 4],
+        "5♣": ["Playing-Cards/card-clubs-5.png", 5], "6♣": ["Playing-Cards/card-clubs-6.png", 6],
+        "7♣": ["Playing-Cards/card-clubs-7.png", 7], "8♣": ["Playing-Cards/card-clubs-8.png", 8],
+        "9♣": ["Playing-Cards/card-clubs-9.png", 9], "10♣": ["Playing-Cards/card-clubs-10.png", 10],
+        "J♣": ["Playing-Cards/card-clubs-11.png", 10], "Q♣": ["Playing-Cards/card-clubs-12.png", 10],
+        "K♣": ["Playing-Cards/card-clubs-13.png", 10], "1♦": ["Playing-Cards/card-diamonds-1.png", 11],
+        "2♦": ["Playing-Cards/card-diamonds-2.png", 2], "3♦": ["Playing-Cards/card-diamonds-3.png", 3],
+        "4♦": ["Playing-Cards/card-diamonds-4.png", 4], "5♦": ["Playing-Cards/card-diamonds-5.png", 5],
+        "6♦": ["Playing-Cards/card-diamonds-6.png", 6], "7♦": ["Playing-Cards/card-diamonds-7.png", 7],
+        "8♦": ["Playing-Cards/card-diamonds-8.png", 8], "9♦": ["Playing-Cards/card-diamonds-9.png", 9],
+        "10♦": ["Playing-Cards/card-diamonds-10.png", 10], "J♦": ["Playing-Cards/card-diamonds-11.png", 10],
+        "Q♦": ["Playing-Cards/card-diamonds-12.png", 10], "K♦": ["Playing-Cards/card-diamonds-13.png", 10]}
 
 keys = list(deck.keys())
 random.shuffle(keys)
@@ -16,13 +40,15 @@ dealer = []
 
 stay_counter = []
 
+
 class SplashWindow(Screen):
-    
+
     def close(self):
-    	exit()
+        exit()
+
 
 class GameScreen(Screen):
-    
+
     def deal_cards(self):
 
         # resets board for more than one match
@@ -41,10 +67,9 @@ class GameScreen(Screen):
         self.ids.card_five.source = ""
         self.ids.card_six.source = ""
 
-
         # clear dealers cards
         self.ids.dealer_card_down.source = ""
-        self.ids.dealer_card_down.height = "0dp"        
+        self.ids.dealer_card_down.height = "0dp"
         self.ids.dealer_card_two.height = "0dp"
         self.ids.dealer_card_two.source = ""
         self.ids.dealer_card_three.height = "0dp"
@@ -55,12 +80,12 @@ class GameScreen(Screen):
         self.ids.dealer_card_five.source = ""
         self.ids.dealer_card_six.height = "0dp"
         self.ids.dealer_card_six.source = ""
-        
+
         # players hand
-        
+
         player.append(deck[keys[0]][1])
         player.append(deck[keys[1]][1])
-        
+
         # show dealer cards
         self.ids.dealer_card_down.source = 'Playing-Cards/card-back2.png'
         self.ids.dealer_card_down.height = "150dp"
@@ -68,7 +93,6 @@ class GameScreen(Screen):
         self.ids.dealer_card_two.source = deck[keys[3]][0]
         self.ids.dealer_card_two.height = "150dp"
         self.ids.dealer_card_two.grow()
-
 
         # show players cards
         self.ids.card_one.source = deck[keys[0]][0]
@@ -79,18 +103,16 @@ class GameScreen(Screen):
         self.ids.card_two.grow()
 
         # dealers hand
-        
-        dealer.append(deck[keys[2]][1]) 
-        dealer.append(deck[keys[3]][1])
 
+        dealer.append(deck[keys[2]][1])
+        dealer.append(deck[keys[3]][1])
 
         self.ids.deal_button.disabled = True
         self.ids.hit_button.disabled = False
         self.ids.stay_button.disabled = False
 
-  
         self.check_score()
-    
+
     def hit(self):
 
         p_hit_count = len(player) + len(dealer)
@@ -119,15 +141,12 @@ class GameScreen(Screen):
             self.ids.hit_button.disabled = False
             self.ids.prompt.text = "You can't draw more than 6 cards!"
 
-        
-        
         self.check_score()
 
     def stay(self):
         stay_counter.append(1)
         self.check_score()
         self.npc()
-
 
     def npc(self):
 
@@ -158,18 +177,14 @@ class GameScreen(Screen):
             else:
                 break
 
-
             dealer_check = sum(dealer)
         stay_counter.append(1)
         self.check_score()
 
-
-   
     def check_score(self):
 
         player_score = sum(player)
         dealer_score = sum(dealer)
-
 
         # Logic for Ace card values
         for values in player:
@@ -186,10 +201,9 @@ class GameScreen(Screen):
 
                 dealer_score = sum(dealer)
 
-
         if len(stay_counter) == 2:
             if player_score == dealer_score:
-                self.ids.prompt.text = "You tied!" 
+                self.ids.prompt.text = "You tied!"
                 self.reset_game()
             elif player_score > dealer_score and player_score < 21:
                 self.ids.prompt.text = "You won!"
@@ -197,7 +211,6 @@ class GameScreen(Screen):
             elif player_score < dealer_score < 21:
                 self.ids.prompt.text = "The Dealer won!"
                 self.reset_game()
-
 
         if player_score == 21 and dealer_score == 21:
             self.ids.prompt.text = "Its a tie at 21!"
@@ -215,10 +228,6 @@ class GameScreen(Screen):
             self.ids.prompt.text = "You won. The Dealer busted 21!"
             self.reset_game()
 
-
- 
-
-   
     def reset_game(self):
 
         # Show the dealers face down card
@@ -230,16 +239,16 @@ class GameScreen(Screen):
         stay_counter.clear()
 
         random.shuffle(keys)
-  
+
         self.ids.deal_button.disabled = False
         self.ids.hit_button.disabled = True
         self.ids.stay_button.disabled = True
-    
+
     # functions for nav bar items
-        
+
     def show_about(self):
-            self.dialog = MDDialog(
-                text="""
+        self.dialog = MDDialog(
+            text="""
 Black Jack app   
 A simple card game    
 
@@ -255,25 +264,22 @@ Credit for Anna-Maria Bergman
 
 Licensed under the: GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 
-""" )
-            self.dialog.open()
-        
-        
+""")
+        self.dialog.open()
+
     def donate_button(self):
-        webbrowser.open('https://ko-fi.com/jessecreates', new = 2)
-        
+        webbrowser.open('https://ko-fi.com/jessecreates', new=2)
+
     def close_two(self):
-    	exit()
-		
+        exit()
+
 
 class NavBar(Screen):
-	pass
-	
+    pass
 
 
 class WindowManager(ScreenManager):
     pass
-
 
 
 class BlackJack(MDApp):
@@ -281,7 +287,6 @@ class BlackJack(MDApp):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "DeepPurple"
         return Builder.load_file('layouts.kv')
-        
 
 
 # on launch start main window class
